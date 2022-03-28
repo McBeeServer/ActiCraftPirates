4cb6de9622ec97af35e32488fc46e1805b7504a0
Mat <silencekrakens@gmail.com>
2022-03-28 07:58:17 +0200

    Added Fly Command

--- src/main/java/net/acticraft/plugin/actipirates/Commands/Gmc.java
+++ src/main/java/net/acticraft/plugin/actipirates/Commands/Gmc.java
@@ -8,16 +8,16 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class Gmc implements CommandExecutor {
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (sender instanceof Player) {
-            Player p = (Player) sender;
-            if (p.hasPermission("acti.gamemode")) {
-                p.setGameMode(GameMode.CREATIVE);
-                p.sendMessage(ChatColor.of("#C5AC7A") + "You changed your gamemode to: " + ChatColor.of("#C5AC7A") + "CREATIVE");
-            }else{
-                sender.sendMessage(ChatColor.of("#C5AC7A") + "You do not have permission to execute this command!");
-            }
+@Override
+public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    if (sender instanceof Player) {
+        Player p = (Player) sender;
+        if (p.hasPermission("acti.gamemode")) {
+            p.setGameMode(GameMode.CREATIVE);
+            p.sendMessage(ChatColor.of("#C5AC7A") + "You changed your gamemode to: " + ChatColor.of("#C5AC7A") + "CREATIVE");
+        }else{
+            sender.sendMessage(ChatColor.of("#C5AC7A") + "You do not have permission to execute this command!");
         }
-        return true;}
+    }
+    return true;}
 }
